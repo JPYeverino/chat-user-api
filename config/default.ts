cfd01f574cf6cab211415356935e3d1e7a943011
JPYeverino <jp.yeverinom@gmail.com>
2019-04-23 16:14:35 -0700

    AWS Ready

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 export default {
     HOST: 'http://auth-api',
     PORT: 3000,
-    MONGO_URI: 'mongodb://mongo:27017/user-api',
+    MONGO_URI: 'mongodb://mongoauth:27017/user-api',
     JWT_KEY: 'MySuperPassword!2345',
     NOTI_API: 'http://noti-api:3001/noti'
 }
